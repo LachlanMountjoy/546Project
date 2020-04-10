94671b51aae625a430da2d6be167d0383296f953
Lachlan <lachlanmountjoy@gmail.com>
2020-04-10 15:24:53 -0400

    basic routing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+const loginRoutes = require('./login');
+const postRoutes = require('./posts');
+const userRoutes = require('./users');
+
 const path = require('path');
 
 const constructorMethod = (app) => {
 	app.get('/', (req, res) => {
-		res.render('layouts/main');
-	});
-
+		res.render('layouts/main', {body :"placeholder"});
+    });
+    app.use('/login', loginRoutes);
+    app.use('/posts', postRoutes);
+    app.use('/users', userRoutes);
 	app.use('*', (req, res) => {
 		res.redirect('/');
 	});
